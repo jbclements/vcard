c6dbc3f212b7a449d96dc41ed265150811bda048
John Clements <clements@racket-lang.org>
2023-01-29 12:58:08 -0800

    vcard work

--- main.rkt
+++ main.rkt
@@ -1,7 +1,9 @@
 #lang racket
 
 (require sugar
-         csv-writing)
+         csv-writing
+         "address-hints-private.rkt"
+         "name-hints-private.rkt")
 
 ;; a vcard is a hash from strings to lists of lists of strings.
 ;; the outer list is because there can be multiple instances of the
@@ -25,9 +27,7 @@
       (match v
         [(list (list "BEGIN:VCARD")
                (list "VERSION:3.0")
-               ;; this will break for newer versions of the OS, I'm just being conservative
-               ;; and exploring
-               (list "PRODID:-//Apple Inc.//macOS 12.6//EN")
+               (list (regexp #px"^PRODID:-//Apple Inc\\.//macOS [.0-9]+//EN$"))
                contents ...
                (list "END:VCARD"))
          (define initial-parse
@@ -47,7 +47,7 @@
                        (map (λ (elt) (cons (second elt) (third elt))) g)))
                (group-by first initial-parse)))]
         [other
-         (error 'zzz "info")])))
+         (error 'parse-vcards "non")])))
 
   (printf "this file appears to contain ~v vcards.\n" (length vcards))
 
@@ -57,9 +57,12 @@
   (define-values (no-name-cards named-cards)
     (partition (λ (card) (equal? (hash-ref card "N") '((";;;;")))) vcards))
 
-  (printf "ignoring these ~v cards that have no N field, all businesses I hope?\n"
-          (length no-name-cards))
-  (pretty-display no-name-cards)
+  (when (not (empty? no-name-cards))
+    (printf "ignoring these ~v cards that have no N field, all businesses I hope?\n"
+            (length no-name-cards))
+    (pretty-display no-name-cards))
+  
+  
 
   (when (ormap (λ (c) (not (= 1 (length (hash-ref c "N"))))) named-cards)
     (error 'not-all-single-named "more info here"))
@@ -83,22 +86,107 @@
 
   unique-named-cards)
 
-(define all-cards (file->unique-named-cards "/tmp/all-cards.vcf"))
+;(define all-cards (file->unique-named-cards "/tmp/all-cards.vcf"))
 (define cc-cards (file->unique-named-cards "/tmp/christmas.vcf"))
 
-(define all-card-names (map card-name all-cards))
+;(define all-card-names (map card-name all-cards))
 (define cc-card-names (map card-name cc-cards))
 
-(unless (subset? cc-card-names all-card-names)
+(define (find-all-addresses card)
+  (define maybe-address-keys
+    
+    (filter (λ (s)
+              (and (regexp-match? #px"ADR" s)
+                   (not (regexp-match #px"^item[0-9]+\\.X-ABADR$" s))))
+            (hash-keys card)))
+  (apply
+   append
+   (apply
+    append
+    (map second
+         (for/list ([k (in-list maybe-address-keys)])
+           (list k (hash-ref card k)))))))
+
+(define (undo-string-quotes s)
+  (regexp-replace* #px"\\\\," s ","))
+
+(define (find-address card)
+  (define addresses
+    (find-all-addresses card))
+  (undo-string-quotes
+   (match addresses
+     ['() ""]
+     [(list just-one-address) just-one-address]
+     [multiple-addresses
+      (match (hash-ref card "FN")
+        [(list (list full-name))
+         (match (assoc full-name address-hints)
+           [(list _ addr-hint)
+            (match (filter (λ (address)
+                             (string-contains? address addr-hint))
+                           multiple-addresses)
+              [(list single-address) single-address]
+              [other (error 'find-address "hint matched more than one address: ~e"
+                            other)])]
+           [#f (error 'find-address "no hint for name ~e with multiple addresses"
+                      full-name)])]
+        [other (error 'zzz "moreinfo")])
+      ])))
+
+(define (address-line-split addr)
+  (match (regexp-split #px";" addr)
+    [(list "") '(empty)]
+    [(list "" "" street city state zip country)
+     (define country-str
+       (match country
+         [(or "" "USA" "United States") ""]
+         ["Canada" ",  Canada"]))
+     (list street
+           (~a city ", " state "  " zip country-str))]))
+
+(define (name-render fn)
+  (match (assoc fn rename-hints)
+    [#f (error 'name-lookup "missing rename for name: ~e" fn)]
+    [(list _ (? string? s)) s]
+    [(list _ 'skip) 'skip]
+    [(list _ 'no-address) (printf "hand-deliver to person with no address: ~e\n"
+                                  fn)
+                          'skip]
+    [(list (list (list n)) 'same) n]
+    [(list (list (list n)) (? symbol? s))
+     (error 'name-lookup "unrecognized behavior symbol ~e for name ~e"
+            s fn)]
+    [other (error "badly formatted : ~e" other)]))
+
+(call-with-output-file "/tmp/christmas.csv"
+  #:exists 'truncate
+  (λ (port)
+    (display-table
+     (filter
+      (λ (row) (string? (first row)))
+      (map
+       (λ (card)
+         (cons (name-render (hash-ref card "FN"))
+               (address-line-split (find-address card))))
+       cc-cards))
+     port)))
+
+#;(unless (subset? cc-card-names all-card-names)
   (error 'zzz "problem1143"))
 
-(for ([n (in-list all-card-names)])
-  (when (not (= (length n) 1))
-    (error 'zz2 "tuhhoth")))
 
 (define cc-card-names-set (list->set cc-card-names))
 
-(call-with-output-file "/tmp/is-christmas.csv"
+#;(call-with-output-file "/tmp/is-christmas.csv"
+  (λ (port)
+    (display-table
+     (for/list ([n (sort all-card-names #:key first string<?)])
+       (list (first n) (cond [(set-member? cc-card-names-set n) "TRUE"]
+                             [else ""])))
+     port)))
+
+
+#;(call-with-output-file "christmas.csv"
   (λ (port)
     (display-table
      (for/list ([n (sort all-card-names #:key first string<?)])
